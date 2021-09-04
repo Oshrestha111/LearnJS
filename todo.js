cdb943612df4f157f3470082d8df287644239904
Oshrestha111 <oriya.shrestha111@gmail.com>
2021-09-04 23:17:02 +0545

    made separate script file

--- todo.js
+++ todo.js
@@ -1,1 +1,13 @@
-function addItems() {}
+function addItems() {
+	var inputItems = document.getElementById("text-bar").value;
+	document.getElementById("error-msg").innerHTML = "";
+	if (inputItems == "") {
+		document.getElementById("error-msg").innerHTML =
+			"Oops! you need to type something.";
+	} else {
+		let list = document.getElementById("listed-items");
+		let li = document.createElement("li");
+		li.textContent = inputItems;
+		list.appendChild(li);
+	}
+}
